9784555c79a20e30f06e75a828c25f09a99c0561
phuong <phuongdenpro@gmail.com>
2022-12-07 00:35:00 +0700

    finish composite pattern

---
+++ composite/src/Leader.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Leader extends Employee{
+    private ArrayList<Employee> employeeArrayList = new ArrayList();
+
+    public Leader(int age, String name, double salary) {
+        super(age, name, salary);
+    }
+
+    @Override
+    public void add(Employee employee) {
+        employeeArrayList.add(employee);
+    }
+
+    @Override
+    public void remove(Employee employee) {
+        employeeArrayList.remove(employee);
+    }
+
+    @Override
+    public void print() {
+        System.out.println("==================");
+        System.out.println("Name : " + this.name);
+        System.out.println("Age: " + this.age);
+        System.out.println("Salary: " + this.salary);
+        System.out.println("==================");
+        for (Employee employee: employeeArrayList) {
+            employee.print();
+        }
+    }
+}
